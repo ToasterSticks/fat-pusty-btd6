35ff81aec5470f428dd5809c034f27ca806c04a4
ToastOnAStick <matt.zhao.18@gmail.com>
2022-06-26 18:39:38 -0400

    fix: forgot to export

--- src/cmds/index.ts
+++ src/cmds/index.ts
@@ -2,5 +2,6 @@ import pingCommand from './ping';
 import redditCommand from './reddit';
 import challengeCommand from './challenge';
 import inviteCommand from './invite';
+import dailyChallengeCommand from './daily';
 
-export default [pingCommand, redditCommand, challengeCommand, inviteCommand];
+export default [pingCommand, redditCommand, challengeCommand, inviteCommand, dailyChallengeCommand];
