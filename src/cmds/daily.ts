f45e2c57462b3d35deccd1bd0c62aa2abe56e2a9
ToastOnAStick <matt.zhao.18@gmail.com>
2022-06-26 18:50:27 -0400

    fix: api returning json

--- src/cmds/daily.ts
+++ src/cmds/daily.ts
@@ -1,10 +1,8 @@
-import { inflate } from 'pako';
 import {
 	APIChatInputApplicationCommandGuildInteraction,
 	APIInteractionResponse,
 	ApplicationCommandOptionType,
 	InteractionResponseType,
-	MessageFlags,
 } from 'discord-api-types/v10';
 
 import { BloonsChallengeData, SlashCommand } from '../types';
@@ -27,39 +25,15 @@ const command: SlashCommand = [
 	}: APIChatInputApplicationCommandGuildInteraction): Promise<APIInteractionResponse> => {
 		const normal_id = Math.trunc((Date.now() / 1000 - 1533974400) / 60 / 60 / 24);
 		const advanced_id = Math.trunc((Date.now() / 1000 - 1535097600) / 60 / 60 / 24);
-
 		const is_advanced =
 			data.options?.[0].type === ApplicationCommandOptionType.Boolean && data.options[0].value;
 		const id = (is_advanced ? advanced_id : normal_id).toString();
 
-		const b64Str = await fetch(
+		const challenge = (await fetch(
 			`https://fast-static-api.nkstatic.com/storage/static/appdocs/11/dailyChallenges${
 				is_advanced ? 'Advanced' : ''
 			}/${id}`
-		).then((res) => res.text());
-
-		let decompressed: string;
-
-		try {
-			decompressed = inflate(
-				new Uint8Array(
-					atob(b64Str)
-						.split('')
-						.map((x) => x.charCodeAt(0))
-				),
-				{ to: 'string' }
-			);
-		} catch {
-			return {
-				type: InteractionResponseType.ChannelMessageWithSource,
-				data: {
-					content: 'The provided challenge code is invalid.',
-					flags: MessageFlags.Ephemeral,
-				},
-			};
-		}
-
-		const challenge: BloonsChallengeData = JSON.parse(decompressed);
+		).then((res) => res.json())) as BloonsChallengeData;
 
 		const embed = generateEmbed(challenge, id, is_advanced ? 'Advanced' : 'Daily');
 
