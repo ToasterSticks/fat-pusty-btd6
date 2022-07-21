4a17d696019cb535a9dcdbb8ae608b2804fd1241
ToastOnAStick <matt.zhao.18@gmail.com>
2022-07-21 13:57:26 -0400

    fix: gold diamond value

--- src/cmds/profile.ts
+++ src/cmds/profile.ts
@@ -122,7 +122,7 @@ const command: SlashCommand = [
 						`<:_:999431043931197480> - ${raceMedals.BlackDiamond ?? 0}`,
 						`<:_:999431045764104203> - ${raceMedals.RedDiamond ?? 0}`,
 						`<:_:999431045260783686> - ${raceMedals.Diamond ?? 0}`,
-						`<:_:999715100694945813> - ${raceMedals.Diamond ?? 0}`,
+						`<:_:999715100694945813> - ${raceMedals.GoldDiamond ?? 0}`,
 					].join('\n'),
 				},
 				{
@@ -132,7 +132,7 @@ const command: SlashCommand = [
 						`<:_:999432157850251314> - ${bossMedals?.BlackDiamond ?? 0}`,
 						`<:_:999432160119369759> - ${bossMedals?.RedDiamond ?? 0}`,
 						`<:_:999432158978527312> - ${bossMedals?.Diamond ?? 0}`,
-						`<:_:999715099575075017> - ${bossMedals?.Diamond ?? 0}`,
+						`<:_:999715099575075017> - ${bossMedals?.GoldDiamond ?? 0}`,
 					].join('\n'),
 				},
 				{
@@ -142,7 +142,7 @@ const command: SlashCommand = [
 						`<:_:999432161348305026> - ${bossEliteMedals?.BlackDiamond ?? 0}`,
 						`<:_:999432165655859290> - ${bossEliteMedals?.RedDiamond ?? 0}`,
 						`<:_:999432164057817138> - ${bossEliteMedals?.Diamond ?? 0}`,
-						`<:_:999714137909235724> - ${bossEliteMedals?.Diamond ?? 0}`,
+						`<:_:999714137909235724> - ${bossEliteMedals?.GoldDiamond ?? 0}`,
 					].join('\n'),
 				},
 				{
