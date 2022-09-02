43860b68d6bc36bbd16f493b4680ee386cf56eec
ToastOnAStick <matt.zhao.18@gmail.com>
2022-09-02 07:09:59 -0400

    fix: temporary removal (maybe permanent)

--- src/cmds/challenge.ts
+++ src/cmds/challenge.ts
@@ -6,8 +6,8 @@ import {
 	MessageFlags,
 } from 'discord-api-types/v10';
 
-import { AuthorizedChallengeData, AuthorizedUserData, BloonsChallengeData } from '../types';
-import { formRequestOptions, generateChallengeEmbed, getOption } from '../util';
+import { BloonsChallengeData } from '../types';
+import { generateChallengeEmbed, getOption } from '../util';
 import { Command } from '../http-interactions';
 
 export const command: Command<ApplicationCommandType.ChatInput> = {
@@ -26,26 +26,9 @@ export const command: Command<ApplicationCommandType.ChatInput> = {
 	handler: async ({ data: { options } }) => {
 		const code = getOption<string>(options, 'code')!.toUpperCase();
 
-		const [b64Str, { results }] = await Promise.all([
-			fetch(`https://static-api.nkstatic.com/appdocs/11/es/challenges/${code}`).then((res) =>
-				res.text()
-			),
-			fetch(
-				'https://api.ninjakiwi.com/utility/es/search',
-				formRequestOptions({
-					index: 'challenges',
-					query: `id:${code}`,
-					limit: 1,
-					offset: 0,
-					hint: 'single_challenge',
-					options: {},
-				})
-			)
-				.then((res) => res.json() as Promise<{ data: string }>)
-				.then(({ data }) => JSON.parse(data) as AuthorizedChallengeData),
-		]);
-
-		const info = results[0] ?? {};
+		const b64Str = await fetch(
+			`https://static-api.nkstatic.com/appdocs/11/es/challenges/${code}`
+		).then((res) => res.text());
 
 		let decompressed: string;
 
@@ -69,45 +52,11 @@ export const command: Command<ApplicationCommandType.ChatInput> = {
 		}
 
 		const challenge: BloonsChallengeData = JSON.parse(decompressed);
-		const creatorID = info.owner;
-
-		if (info)
-			await Promise.all(
-				(
-					[
-						['owner', info.owner],
-						['firstWin', info.stats.firstWin?.slice(3)],
-						['latestWin', info.stats.latestWin?.slice(3)],
-					] as const
-				).map(async ([key, value]) => {
-					if (!value && key !== 'owner') {
-						info.stats[key] = '';
-						return;
-					}
-
-					const { users } = await fetch(
-						'https://api.ninjakiwi.com/user/search',
-						formRequestOptions({
-							method: 'nkapiID',
-							keys: [value],
-							includeOnlineStatus: false,
-						})
-					)
-						.then((res) => res.json() as Promise<{ data: string }>)
-						.then(({ data }) => JSON.parse(data) as AuthorizedUserData);
-
-					if (key === 'owner') info[key] = Object.values(users)[0].displayName;
-					else info.stats[key] = Object.values(users)[0].displayName;
-				})
-			);
 
 		const embed = generateChallengeEmbed({
 			data: challenge,
 			id: code,
-			info,
 		});
-
-		embed.footer = { text: creatorID };
 
 		return {
 			type: InteractionResponseType.ChannelMessageWithSource,
